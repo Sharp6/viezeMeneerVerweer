9435e7c419ef324794a8184fb5f6cc55dacd2163
Philip Huysmans <philip.huysmans@ua.ac.be>
2015-12-21 15:14:35 +0100

    added menu functionality

--- GAME_menu.py
+++ GAME_menu.py
@@ -9,11 +9,18 @@ class Menu():
 		self.windowHeight = windowHeight
 		self.input = inputDevice
 
-		self.menuItemLabels = [ "Speler", "Moeilijk", "Start!" ]
+		self.menuItemLabels = [ "Speler", "Moeilijkheid", "Start!" ]
 		self.currentMenuItem = 2
 		self.menuPosX = self.windowWidth / 2
 		self.menuPosY = 20
 		self.menuSpacing = 80
+		self.menuPaddingX = 100
+
+		self.playerLabels = [ "LOTTE", "MARIE" ]
+		self.currentPlayer = 0
+
+		self.difficultyLabels = [ "Makkelijk", "Moeilijk", "Moeilijkst" ]
+		self.currentDifficulty = 0
 
 		self.selectionBoxWidth = 300
 		self.selectionBoxHeight = 30
@@ -24,6 +31,8 @@ class Menu():
 
 		self.tickOfLastCommand = 0
 		self.commandInterval = 1000
+
+		self.textColor = (10, 10, 10)
 
 	def enter(self):
 		print "Entering MENU state."
@@ -42,7 +51,12 @@ class Menu():
 				else:
 					self.increaseSelection()
 		if buttonState:
-			self.fsm.changeState(self.nextState)
+			if currentMenuItem == 2:
+				self.fsm.changeState(self.nextState)
+			elif currentMenuItem == 0:
+				self.changePlayer()
+			elif currentMenuItem == 1:
+				self.changeDifficulty()
 
 	def draw(self):
 		self.surface.blit(self.background, (0,0))
@@ -52,15 +66,21 @@ class Menu():
 	def loadImages(self):
 		self.background = self.pygame.image.load("assets/menu.png")
 
+	def drawMenu(self):
+		self.drawLabels()
+		self.drawPlayer()
+		self.drawDifficulty()
+
 	def drawLabels(self):
 		for label in self.menuItemLabels:
 			self.drawLabel(label)
 
 	def drawLabel(self,label):
-		labelText = self.font.render(str(label), 1, (10, 10, 10))
+		labelText = self.font.render(str(label), 1, self.textColor)
 		labelPos = labelText.get_rect()
 		labelPos.centerx = self.menuPosX
 		labelPos.centery = self.menuPosY + self.menuItemLabels.index(label) * self.menuSpacing
+
 		self.surface.blit(labelText,labelPos)
 
 	def drawSelectionBox(self,label):
@@ -74,3 +94,23 @@ class Menu():
 		self.currentMenuItem = self.currentMenuItem - 1
 		if self.currentMenuItem < 0:
 			self.currentMenuItem = len(self.menuItemLabels) - 1
+
+	def drawPlayer(self):
+		playerText = self.font.render(str(self.playerLabels[self.currentPlayer]), 1, self.textColor)
+		playerPos = playerText.get_rect()
+		playerPos.centery = self.menuPosY + self.menuItemLabels.index("Speler") * self.menuSpacing
+		pplayerPos.centerx = self.menuPoxX + self.menuPaddingX
+
+	def drawDifficulty(self):
+		difficultyText = self.font.render(str(self.difficultyLabels[self.currentDifficulty]), 1, self.textColor)
+		difficultyPos = difficultyText.get_rect()
+		difficultyPos.centery = self.menuPosY + self.menuItemLabels.index("Moeilijkheid") * self.menuSpacing
+		difficultyPos.centerx = self.menuPoxX + self.menuPaddingX
+
+	def changePlayer(self):
+		self.currentPlayer = self.currentPlayer+1
+		self.currentPlayer = self.currentPlayer % len(self.playerLabels)
+
+	def changeDifficulty(self):
+		self.currentDifficulty = self.currentDifficulty + 1
+		self.currentDifficulty = self.currentDifficulty % len(self.difficultyLabels)
